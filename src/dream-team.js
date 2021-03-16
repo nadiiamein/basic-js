dc8d2ebfadf9e6e53f28d1787818887e8998678d
Nadiia <npodufala15@gmail.com>
2021-03-16 15:27:00 +0200

    feat: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let codM = '';
+  members.map(m => {
+    if(typeof m === "string") {
+      m = m.trim().toUpperCase();
+      codM = codM + m.slice(0, 1);
+    }
+  })
+  return codM.split('').sort().join('')
 };
