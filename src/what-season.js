9a31730f16dc5b3f455d8991a01ff896ad98f4dd
Nadiia <npodufala15@gmail.com>
2021-03-25 22:18:06 +0200

    what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,11 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+    if (date === undefined) {
+      return 'Unable to determine the time of year!';
+    }  
+    let monat = date.getMonth();
+    if(!(date instanceof Date && !isNaN(date.valueOf()))) throw Error;
+    if (monat >= 2 && monat <= 4) {return 'spring'}
+    else if (monat >= 5 && monat <= 7) return 'summer';
+    else if (monat >= 8 && monat <= 10) return 'autumn';
+    else return 'winter';
+  }
