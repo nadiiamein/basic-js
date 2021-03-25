4329c753fad04d2f1498f3a464ac7fabae1dd8b9
Nadiia <npodufala15@gmail.com>
2021-03-25 22:53:27 +0200

    ex-repet

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+    if(typeof str != 'string') 
+    {
+    str = String(str);
+  }
+  let res = [];
+  let addRes = [];
+
+  if(!options.separator) {
+    options.separator = "+";}
+
+  if(options.hasOwnProperty("addition")) {
+    if(typeof options.addition != 'string') {
+      options.addition = String(options.addition);
+    }
+    if(!options.additionSeparator) {
+      options.additionSeparator = "|";
+    }
+    for(let j = 0; j < options.additionRepeatTimes - 1; j += 1) {
+      addRes.push(options.addition);
+      addRes.push(options.additionSeparator);
+    }
+    addRes.push(options.addition);
+    addRes.push(str);
+    str = addRes.reverse().join('');
+  }
+for(let i = 0; i < options.repeatTimes - 1; i += 1) 
+{
+  res.push(str);
+  res.push(options.separator);
+}
+res.push(str);
+  return res.join('');
 };
   
