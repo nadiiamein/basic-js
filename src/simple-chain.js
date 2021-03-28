4478ead9e81ada0443dc865d85333809eaedd740
Nadiia <npodufala15@gmail.com>
2021-03-28 14:05:40 +0300

    simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
+let arr = [];
+
 const chainMaker = {
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return arr.length;
+    
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    
+    if(value === null) value = 'null';
+    arr.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  
+    if(position < 1 || position > arr.length) {
+      arr = [];
+      throw new Error();
+    }
+    arr.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    arr = arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    let res = arr;
+    arr = [];
+    return '( '+ res.join(' )~~( ') +' )';
   }
 };
 
