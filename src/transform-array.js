c0c57a4f1a9c8837ff22c47e2bc4eb6efa71c87a
Nadiia <npodufala15@gmail.com>
2021-03-28 23:11:32 +0300

    transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw Error 
+  const array = [...arr];
+  for (let i = 0; i < array.length; i++) {
+    let el = array[i];
+
+    if(el === '--double-next') {
+      if (typeof array[i + 1] === 'undefinded') {
+        array[i] = null;
+      } else {
+        array[i] = array[i +1]
+      }
+      i -= 1
+      continue;
+    }
+    if (el === '--double-prev') {
+      if (typeof array[i - 1] === 'undefined') {
+        array[i] = null
+      } else{
+        array[i] = array[i - 1]
+      }
+
+    }
+    if (el === '--discard-next') {
+      if (typeof array [i +1] === 'undefined') {
+        array[i] = null 
+      } else {
+        array[i+ 1] = null
+        array[i] = null
+      }
+    }
+    if (el === '--discard-prev') {
+      if(typeof array[i -1] === 'undefined' || array[i-1] === null) {
+        array[i] = null
+      } else {
+        array[i -1] = null
+        array[i] = null
+      }
+    }
+  }
+  return [...array.filter(el => el !=null)];
 };
